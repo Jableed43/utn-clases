bedf00879cf50b5a49ec1ab393b5fb6f95241ac4
Jableed43 <nlopez@ticmas.com>
2023-11-10 08:38:00 -0300

    clase 14

---
+++ clase-14/includes.js
@@ -0,0 +1,10 @@
+let animales = ['perro', 'avestruz', 'caballo']
+console.log(animales.includes('perro')) //true
+
+let mascotas = [['perro', 'marron'], ['gato', 'blanco'], ['cobayo', 'negro']] 
+console.log(mascotas[2][1]) //negro
+
+//año
+console.log(new Date().getFullYear())
+//dia
+console.log(new Date().getDate())
